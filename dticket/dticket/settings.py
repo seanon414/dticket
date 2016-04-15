fcffb1bbdace4001b05945092413c409ce263868
Sean O'Neill <sean.o'neill@dealertrack.com>
2016-04-15 20:44:14 +0000

    Creating index page and connecting Irene garu's style sheets

--- dticket/dticket/settings.py
+++ dticket/dticket/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'dticket.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [''],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -98,5 +98,10 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+    '/dticket/static/',
+]
 
 STATIC_URL = '/static/'
