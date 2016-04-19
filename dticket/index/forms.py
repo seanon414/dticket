d4ff17c9d1c870bb01f1ec593bc107be5dcb207d
Sean O'Neill <sean.o'neill@dealertrack.com>
2016-04-19 14:34:00 +0000

    Adding in slack bot functionality

--- dticket/index/forms.py
+++ dticket/index/forms.py
@@ -1,5 +1,16 @@
 from django import forms
+from django.forms.fields import IntegerField, CharField
 
 
 class HomePageForm(forms.Form):
-    ticket_number = forms.HiddenInput()
+    ticket_number = IntegerField(required=False,
+                                 initial=0,
+                                 min_value=0,
+                                 widget=forms.HiddenInput())
+
+    ticket_id = CharField(required=False)
+
+    slack_username = CharField(required=False)
+
+    def __init__(self, *args, **kwargs):
+        super(HomePageForm, self).__init__(*args, **kwargs)
--- dticket/index/views.py
+++ dticket/index/views.py
@@ -1,25 +1,34 @@
-from django.shortcuts import render
+from django.shortcuts import render, render_to_response
 from django.views.generic import FormView
 
 from index.forms import HomePageForm
 
 from slacker import Slacker
 
+slack = Slacker('xoxb-35700221924-XjG9qR4swYgOf4uEIWy1F7JG')
+
 
 class HomePageView(FormView):
     template_name = "templates/index.html"
     form_class = HomePageForm
-    ticket_number = 0
     success_url = '/'
 
-    def form_valid(self, form):
-        return super(HomePageView, self).form_valid(form)
+    def post(self, request, *args, **kwargs):
+        form_class = self.get_form_class()
+        form = self.get_form(form_class)
+        if form.is_valid():
+            if 'button_slack' in form.data:
+                self.slack_me()
+            return self.form_valid(form, **kwargs)
+        else:
+            return self.form_invalid(form, **kwargs)
 
-    def increase_ticket_number(self):
-        ticket_number += 1
-
-    def decrease_ticket_number(self):
-        ticket_number -= 1
+    def get(self, request, *args, **kwargs):
+        form_class = self.get_form_class()
+        form = self.get_form(form_class)
+        context = self.get_context_data(**kwargs)
+        context['form'] = form
+        return self.render_to_response(context)
 
     def slack_me(self):
-        pass
+        slack.chat.post_message('#d-ticket-hack', 'Hello! We will notify you when your ticket is called.')
