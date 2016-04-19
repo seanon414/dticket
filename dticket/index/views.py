2bd69a21f2fc2d8e30a71239345e9e2df47d49a1
Sean O'Neill <sean.o'neill@dealertrack.com>
2016-04-19 15:17:29 +0000

    Adding next button slack message

--- dticket/index/views.py
+++ dticket/index/views.py
@@ -17,8 +17,13 @@ class HomePageView(FormView):
         form_class = self.get_form_class()
         form = self.get_form(form_class)
         if form.is_valid():
+            slack_username = form.cleaned_data['slack_username']
+            ticket_id = form.cleaned_data['ticket_id']
             if 'button_slack' in form.data:
-                self.slack_me()
+                self.slack_initial_message(slack_username, ticket_id)
+            if 'button_next' in form.data:
+                pass
+                self.slack_ticket_called(slack_username, ticket_id)
             return self.form_valid(form, **kwargs)
         else:
             return self.form_invalid(form, **kwargs)
@@ -30,5 +35,10 @@ class HomePageView(FormView):
         context['form'] = form
         return self.render_to_response(context)
 
-    def slack_me(self):
-        slack.chat.post_message('#d-ticket-hack', 'Hello! We will notify you when your ticket is called.')
+    def slack_initial_message(self, user_name, ticket_id):
+        slack_string = 'Hello @{}! We will notify you when your ticket, {}, is called.'.format(user_name, ticket_id)
+        slack.chat.post_message('#d-ticket-hack', slack_string)
+
+    def slack_ticket_called(self, user_name, ticket_id):
+        slack_string = '@{}! Your ticket, {}, has been called.'.format(user_name, ticket_id)
+        slack.chat.post_message('#d-ticket-hack', slack_string) 
